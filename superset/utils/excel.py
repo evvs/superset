36ae1a7fc9654d717a445f2fce562d29f625cd29
Evgeniy Valyaev <va.evgeny@gmail.com>
2023-07-07 15:56:35 +0300

    fix date_format for renaming pandas columns

--- superset/utils/excel.py
+++ superset/utils/excel.py
@@ -26,24 +26,34 @@ import numpy as np
 import logging
 logger = logging.getLogger(__name__)
 
+
 def df_to_excel(df: pd.DataFrame, sheet_name='Sheet1', from_report=False, slice: Union[Slice, None] = None, **kwargs: Any) -> bytes:
     output = io.BytesIO()
 
     # Normalize and format datetime columns
 
-    try:
-        if (slice):
-            for col in df.select_dtypes(include=[np.datetime64, 'datetime']).columns:
-                col_name = df[col].name
-                if isinstance(col_name, tuple):
-                    col_name=col_name[0]
-                date_format = slice.form_data.get("column_config").get(col_name).get("d3TimeFormat")
-                if (date_format and date_format != "smart_date"):
-                    df[col] = df[col].dt.strftime(date_format)
-    except BaseException as err:
-        logger.error("ERROR WHEN TRYING FORMAT DATE")
-        logger.error(err)
+    if (slice):
+        try:
+            for idx, column in enumerate(df.columns):
+                if df[column].dtype in ['datetime64[ns]', 'datetime']:
+                    col_name = column if not isinstance(column, tuple) else column[0]
+                    col_config = slice.form_data.get("column_config", {}).get(col_name) #.get("d3TimeFormat")
 
+                    if (not col_config):
+                        mapped_field = ''
+                        all_columns = slice.form_data.get("all_columns")
+                        if (all_columns):
+                            mapped_field = all_columns[idx]
+                        col_config =  slice.form_data.get("column_config", {}).get(mapped_field)
+
+                    date_format = col_config.get("d3TimeFormat")
+
+                    if date_format and date_format != "smart_date":
+                        df[column] = df[column].dt.strftime(date_format)
+        except Exception as err:
+            logger.error(f"ERROR WHEN TRYING FORMAT DATE for column: {column}")
+            logger.error(err)
+    
     with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
         df.to_excel(writer, sheet_name=sheet_name, **kwargs)
 
@@ -69,7 +79,8 @@ def df_to_excel(df: pd.DataFrame, sheet_name='Sheet1', from_report=False, slice:
             if (slice.datasource.type):
                 try:
                     chart_name = str(slice.slice_name)
-                    chart = GenerateChart(df, workbook, worksheet, chart_name)  # type: ignore
+                    chart = GenerateChart(
+                        df, workbook, worksheet, chart_name)  # type: ignore
                     chart_type = slice.form_data.get("viz_type")
                     if (chart_type):
                         chart.generate(chart_type)
